2b6137021e42c040976d55d39bdec7c8ffdf81ee
Guan <guan.shiliang@gmail.com>
2026-08-23 03:42:18 +0800

    fix: component images build FROM local mailez/base:local instead of docker.io library/base

--- deploy/scripts/build-images.ps1
+++ deploy/scripts/build-images.ps1
@@ -33,7 +33,7 @@ if ($LASTEXITCODE -ne 0) { throw 'docker daemon is not running' }
 
 function Build-Image([string]$name, [string]$dir) {
   Write-Host "==> building mailez/$name :local from $dir"
-  & $docker build --build-arg VERSION=local -t "mailez/$name`:local" $dir
+  & $docker build --build-arg VERSION=local --build-arg BASE_IMAGE="mailez/base:local" -t "mailez/$name`:local" $dir
   if ($LASTEXITCODE -ne 0) { throw "build failed: mailez/$name" }
 }
 
